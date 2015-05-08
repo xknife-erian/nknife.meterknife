0192c33e166ca2eb415ce6ac54045ed89f8b14d1
lukan@xknife.net <lukan@xknife.net>
2015-05-08 16:18:42 +0800

    开始制作BaseParamPanel，由这个控件关联命令的配制，及测量参数

---
+++ MeterKnife.Instruments/Agilent/Ag34401AParamPanel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MeterKnife.Instruments.Agilent
+{
+    public partial class Ag34401AParamPanel : UserControl
+    {
+        public Ag34401AParamPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void Ag34401AParamPanel_Load(object sender, EventArgs e)
+        {
+
+        }
+
+    }
+}
