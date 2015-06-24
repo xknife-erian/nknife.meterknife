0efcef31690afc52dc811817822e4e150d6ef017
lukan@xknife.net <lukan@xknife.net>
2015-06-25 01:15:07 +0800

    开发过程，小阶段提交.../lukan

--- MeterKnife.Common/Algorithms/Difference/StandardDeviation.cs
+++ MeterKnife.Common/Algorithms/Difference/StandardDeviation.cs
@@ -2,10 +2,20 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using MeterKnife.Common.Interfaces;
 
 namespace MeterKnife.Common.Algorithms
 {
-    class StandardDeviation
+    /// <summary>
+    /// 标准差
+    /// </summary>
+    public class StandardDeviation : IElectronDifferenceAlgorithm
     {
+        public double NominalValue { get; set; }
+        public IElectronAlgorithm Original { get; set; }
+        public double Output()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
