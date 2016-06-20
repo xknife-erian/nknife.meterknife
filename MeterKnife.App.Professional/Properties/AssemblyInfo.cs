fae414e05cb1d6b75defb1a08bb23ecbd3b34a86
lukan@xknife.net <7uu7uicbc33442>
2016-06-20 09:26:42 +0800

    清理解决方案

--- MeterKnife.App.Professional/Properties/AssemblyInfo.cs
+++ MeterKnife.App.Professional/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyCompany("xknife.net")]
 [assembly: AssemblyCopyright("Copyright © xknife.net 2016")]
-[assembly: AssemblyProduct("MeterKnife 2015.")]
-[assembly: AssemblyDescription("本软件是开源软件。本软件可广泛应用于电子行业的仪器的计算机控制，软件能够进行高性能的测试/检查系统。可用于万用表，电源，频率计，示波器等仪器，您可以很容易地利用它建立自动检查与测试系统。这能够使您将传统的目视检查处理过程转换到基于计算机的自动化测试。")]
+[assembly: AssemblyProduct("MeterKnife 2016.")]
+[assembly: AssemblyDescription("本软件是开源软件。本软件可广泛应用于电子行业的测试与测量仪器的计算机控制，软件能够进行高性能的检查与测试。可用于万用表，电源，频率计，示波器等仪器，您可以很容易地利用它建立自动检查与测试系统。这能够使您将传统的目视检查处理过程转换到基于计算机的自动化测试。可让您的仪器发挥最大功效，进行深入的设计分析，并可节省测试时间。本软件致力于在多种PC连接环境下提供快速方便的不同品牌仪器连接、命令控制、数据采集与分析。")]
 [assembly: ComVisible(false)]
 [assembly: InternalsVisibleTo("MeterKnife.UnitTests")]
 
-[assembly: AssemblyVersion("1.0.3.0619")]
-[assembly: AssemblyFileVersion("1.0.3.0619")]
+[assembly: AssemblyVersion("1.99.0.0620")]
+[assembly: AssemblyFileVersion("1.99.0.0620")]
