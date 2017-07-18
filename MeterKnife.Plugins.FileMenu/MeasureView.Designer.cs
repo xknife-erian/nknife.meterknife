c2fc249a543a137199109123ee281ddb3b870dd5
lukan@xknife.net <lukan@live.cn>
2017-07-18 15:04:06 +0800

    ...

--- MeterKnife.Plugins.FileMenu/MeasureView.Designer.cs
+++ MeterKnife.Plugins.FileMenu/MeasureView.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // MeasureView
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 441);
+            this.Name = "MeasureView";
             this.Text = "MeasureView";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- MeterKnife.Plugins.FileMenu/MeasureView.cs
+++ MeterKnife.Plugins.FileMenu/MeasureView.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using NKnife.IoC;
 using WeifenLuo.WinFormsUI.Docking;
 using IExtenderProvider = MeterKnife.Interfaces.Plugins.IExtenderProvider;
 
