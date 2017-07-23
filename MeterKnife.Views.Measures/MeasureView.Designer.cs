48497f82cdfd797cb1ece4b9100510c20a09f3ce
lukan@xknife.net <7uu7uicbc33442>
2017-07-23 10:25:20 +0800

    设计图表展示窗体过程中....

--- MeterKnife.Views.Measures/MeasureView.Designer.cs
+++ MeterKnife.Views.Measures/MeasureView.Designer.cs
@@ -31,82 +31,105 @@ namespace MeterKnife.Views.Measures
         /// </summary>
         private void InitializeComponent()
         {
-            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this._MeasurePlotPage = new System.Windows.Forms.TabPage();
             this._PlotView = new OxyPlot.WindowsForms.PlotView();
-            this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1.SuspendLayout();
-            this.tabControl1.SuspendLayout();
+            this._TabControl = new System.Windows.Forms.TabControl();
+            this._ToolStripContainer = new System.Windows.Forms.ToolStripContainer();
+            this._ToolStrip = new System.Windows.Forms.ToolStrip();
+            this._MeasurePlotPage.SuspendLayout();
+            this._TabControl.SuspendLayout();
+            this._ToolStripContainer.ContentPanel.SuspendLayout();
+            this._ToolStripContainer.TopToolStripPanel.SuspendLayout();
+            this._ToolStripContainer.SuspendLayout();
             this.SuspendLayout();
             // 
-            // toolStrip1
+            // _MeasurePlotPage
             // 
-            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(728, 25);
-            this.toolStrip1.TabIndex = 2;
-            this.toolStrip1.Text = "toolStrip1";
-            // 
-            // tabPage1
-            // 
-            this.tabPage1.Controls.Add(this._PlotView);
-            this.tabPage1.Location = new System.Drawing.Point(4, 4);
-            this.tabPage1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.tabPage1.Size = new System.Drawing.Size(720, 426);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
+            this._MeasurePlotPage.BackColor = System.Drawing.Color.DarkGray;
+            this._MeasurePlotPage.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this._MeasurePlotPage.Controls.Add(this._PlotView);
+            this._MeasurePlotPage.Location = new System.Drawing.Point(4, 4);
+            this._MeasurePlotPage.Name = "_MeasurePlotPage";
+            this._MeasurePlotPage.Padding = new System.Windows.Forms.Padding(3, 3, 3, 3);
+            this._MeasurePlotPage.Size = new System.Drawing.Size(616, 385);
+            this._MeasurePlotPage.TabIndex = 0;
+            this._MeasurePlotPage.Text = "实时测量";
             // 
             // _PlotView
             // 
-            this._PlotView.BackColor = System.Drawing.Color.White;
+            this._PlotView.BackColor = System.Drawing.Color.DarkGray;
             this._PlotView.Dock = System.Windows.Forms.DockStyle.Fill;
-            this._PlotView.Location = new System.Drawing.Point(4, 3);
-            this._PlotView.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this._PlotView.Location = new System.Drawing.Point(3, 3);
             this._PlotView.Name = "_PlotView";
             this._PlotView.PanCursor = System.Windows.Forms.Cursors.Hand;
-            this._PlotView.Size = new System.Drawing.Size(712, 420);
+            this._PlotView.Size = new System.Drawing.Size(608, 377);
             this._PlotView.TabIndex = 1;
             this._PlotView.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
             this._PlotView.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
             this._PlotView.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
             // 
-            // tabControl1
+            // _TabControl
             // 
-            this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Bottom;
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tabControl1.Location = new System.Drawing.Point(0, 25);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(728, 452);
-            this.tabControl1.TabIndex = 3;
+            this._TabControl.Alignment = System.Windows.Forms.TabAlignment.Bottom;
+            this._TabControl.Controls.Add(this._MeasurePlotPage);
+            this._TabControl.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._TabControl.ItemSize = new System.Drawing.Size(60, 22);
+            this._TabControl.Location = new System.Drawing.Point(0, 0);
+            this._TabControl.Name = "_TabControl";
+            this._TabControl.SelectedIndex = 0;
+            this._TabControl.Size = new System.Drawing.Size(624, 415);
+            this._TabControl.TabIndex = 3;
+            // 
+            // _ToolStripContainer
+            // 
+            // 
+            // _ToolStripContainer.ContentPanel
+            // 
+            this._ToolStripContainer.ContentPanel.Controls.Add(this._TabControl);
+            this._ToolStripContainer.ContentPanel.Size = new System.Drawing.Size(624, 415);
+            this._ToolStripContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this._ToolStripContainer.Location = new System.Drawing.Point(0, 0);
+            this._ToolStripContainer.Name = "_ToolStripContainer";
+            this._ToolStripContainer.Size = new System.Drawing.Size(624, 440);
+            this._ToolStripContainer.TabIndex = 4;
+            this._ToolStripContainer.Text = "toolStripContainer1";
+            // 
+            // _ToolStripContainer.TopToolStripPanel
+            // 
+            this._ToolStripContainer.TopToolStripPanel.Controls.Add(this._ToolStrip);
+            // 
+            // _ToolStrip
+            // 
+            this._ToolStrip.Dock = System.Windows.Forms.DockStyle.None;
+            this._ToolStrip.Location = new System.Drawing.Point(3, 0);
+            this._ToolStrip.Name = "_ToolStrip";
+            this._ToolStrip.Size = new System.Drawing.Size(43, 25);
+            this._ToolStrip.TabIndex = 0;
             // 
             // MeasureView
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(728, 477);
-            this.Controls.Add(this.tabControl1);
-            this.Controls.Add(this.toolStrip1);
-            this.Font = new System.Drawing.Font("Verdana", 8.25F);
-            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.ClientSize = new System.Drawing.Size(624, 440);
+            this.Controls.Add(this._ToolStripContainer);
             this.Name = "MeasureView";
             this.Text = "MeasureView";
-            this.tabPage1.ResumeLayout(false);
-            this.tabControl1.ResumeLayout(false);
+            this._MeasurePlotPage.ResumeLayout(false);
+            this._TabControl.ResumeLayout(false);
+            this._ToolStripContainer.ContentPanel.ResumeLayout(false);
+            this._ToolStripContainer.TopToolStripPanel.ResumeLayout(false);
+            this._ToolStripContainer.TopToolStripPanel.PerformLayout();
+            this._ToolStripContainer.ResumeLayout(false);
+            this._ToolStripContainer.PerformLayout();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-        private ToolStrip toolStrip1;
-        private TabPage tabPage1;
+        private TabPage _MeasurePlotPage;
         private PlotView _PlotView;
-        private TabControl tabControl1;
+        private TabControl _TabControl;
+        private ToolStripContainer _ToolStripContainer;
+        private ToolStrip _ToolStrip;
     }
 }
