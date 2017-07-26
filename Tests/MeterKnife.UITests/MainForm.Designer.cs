c5d7799d66bba5ed0fa39bb0c76b5f3e36a28048
lukan@xknife.net <lukan@live.cn>
2017-07-26 11:05:28 +0800

    新建界面测试项目

---
+++ Tests/MeterKnife.UITests/MainForm.Designer.cs
@@ -0,0 +1,75 @@
+﻿namespace MeterKnife.UITests
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this._MenuStrip = new System.Windows.Forms.MenuStrip();
+            this._StatusStrip = new System.Windows.Forms.StatusStrip();
+            this.SuspendLayout();
+            // 
+            // _MenuStrip
+            // 
+            this._MenuStrip.Location = new System.Drawing.Point(0, 0);
+            this._MenuStrip.Name = "_MenuStrip";
+            this._MenuStrip.Size = new System.Drawing.Size(842, 24);
+            this._MenuStrip.TabIndex = 0;
+            this._MenuStrip.Text = "menuStrip1";
+            // 
+            // _StatusStrip
+            // 
+            this._StatusStrip.Location = new System.Drawing.Point(0, 476);
+            this._StatusStrip.Name = "_StatusStrip";
+            this._StatusStrip.Size = new System.Drawing.Size(842, 22);
+            this._StatusStrip.TabIndex = 1;
+            this._StatusStrip.Text = "statusStrip1";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(842, 498);
+            this.Controls.Add(this._StatusStrip);
+            this.Controls.Add(this._MenuStrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this._MenuStrip;
+            this.MinimumSize = new System.Drawing.Size(120, 148);
+            this.Name = "MainForm";
+            this.Text = "MeterKnife UI Test Form";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip _MenuStrip;
+        private System.Windows.Forms.StatusStrip _StatusStrip;
+    }
+}
+
---
+++ Tests/MeterKnife.UITests/MainForm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using NKnife.ControlKnife;
+using NKnife.IoC;
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace MeterKnife.UITests
+{
+    public partial class MainForm : SimpleForm
+    {
+        private readonly DockPanel _DockPanel = new DockPanel();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeDockPanel();
+        }
+
+        private void InitializeDockPanel()
+        {
+            _DockPanel.Theme = new VS2015BlueTheme();
+            _DockPanel.Dock = DockStyle.Fill;
+            Controls.Add(_DockPanel);
+            _DockPanel.BringToFront();
+        }
+    }
+}
