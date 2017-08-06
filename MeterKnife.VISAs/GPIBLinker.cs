4855308b72dc79b3181bc15e52e6936ebeaab285
lukan@xknife.net <7uu7uicbc33442>
2017-08-07 00:12:31 +0800

    ....

--- MeterKnife.VISAs/GPIBLinker.cs
+++ MeterKnife.VISAs/GPIBLinker.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.Threading;
-using VisaComLib;
+using Ivi.Visa.Interop;
 
 namespace MeterKnife.VISAs
 {
@@ -8,14 +8,22 @@ namespace MeterKnife.VISAs
     {
         private IFormattedIO488 _Gpib;
 
+        public GPIBLinker(Action<string> loggerAction, short gpibSelector, short address)
+        {
+            LoggerAction = loggerAction;
+            GpibSelector = gpibSelector;
+            Address = address;
+        }
+
         public short GpibSelector { get; set; }
         public short Address { get; set; }
-        public string Option { get; set; }
-        public int OpenTimeout { get; set; }
+        public string Option { get; set; } = string.Empty;
+        public int OpenTimeout { get; set; } = 2000;
+        public Action<string> LoggerAction { get; set; }
 
-        public string Initialize()
+        public bool Initialize(out string idn)
         {
-            string fullAddress = $"GPIB{GpibSelector}::{Address}::INSTR";
+            string openCommand = $"GPIB{GpibSelector}::{Address}::INSTR";
 
             try
             {
@@ -25,20 +33,30 @@ namespace MeterKnife.VISAs
                 //create the resource manager
                 var mgr = new ResourceManager();
 
-                _Gpib.IO = (IMessage)mgr.Open(fullAddress, AccessMode.NO_LOCK, OpenTimeout, Option);
+                _Gpib.IO = (IMessage)mgr.Open(openCommand, AccessMode.NO_LOCK, OpenTimeout, Option);
+                LoggerAction.Invoke($"VISA.SET: {openCommand}");
 
                 _Gpib.WriteString("*CLS", true);
                 Thread.Sleep(500);
 
                 _Gpib.WriteString("*IDN?", true);
-                var value = _Gpib.ReadString();
+                idn = _Gpib.ReadString();
 
-                return value;
+                return true;
             }
             catch (Exception e)
             {
-                return "";
+                LoggerAction.Invoke($"ERROR:{e.Message}");
+                idn = string.Empty;
+                return false;
             }
+        }
+
+        public string Execute(string scpiCommand, uint timeOut, bool flushAndEnd = true)
+        {
+            _Gpib.WriteString(scpiCommand, flushAndEnd);
+            Thread.Sleep((int) timeOut);
+            return _Gpib.ReadString();
         }
     }
 }
