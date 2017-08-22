a93c1a3bf201c203d721dd947d577a872103b968
lukan@xknife.net <lukan@live.cn>
2017-08-22 17:08:01 +0800

    ....

---
+++ MeterKnife.Views.InstrumentsDiscovery/InstrumentsDetailCell.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MeterKnife.Views.InstrumentsDiscovery
+{
+    public partial class InstrumentsDetailCell : UserControl
+    {
+        public InstrumentsDetailCell()
+        {
+            InitializeComponent();
+        }
+
+        public Image Image
+        {
+            get => _PictureBox.Image;
+            set => _PictureBox.Image = value;
+        }
+
+        public string Model
+        {
+            get => _ModelLabel.Text;
+            set => _ModelLabel.Text = value;
+        }
+
+        public string Manufacturer
+        {
+            get => _ManufacturerLabel.Text;
+            set => _ManufacturerLabel.Text = value;
+        }
+
+        public string ConnString
+        {
+            get => _ConnStringLabel.Text;
+            set => _ConnStringLabel.Text = value;
+        }
+
+        public string Information
+        {
+            get => _InformationLabel.Text;
+            set => _InformationLabel.Text = value;
+        }
+
+        public string DatasCount
+        {
+            get => _DatasCountLabel.Text;
+            set => _DatasCountLabel.Text = value;
+        }
+
+        public string UsingTime
+        {
+            get => _UsingTimeLabel.Text;
+            set => _UsingTimeLabel.Text = value;
+        }
+    }
+}
--- MeterKnife.Views.InstrumentsDiscovery/InstrumentsPanel.Designer.cs
+++ MeterKnife.Views.InstrumentsDiscovery/InstrumentsPanel.Designer.cs
@@ -28,18 +28,62 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.instrumentsDetailCell1 = new MeterKnife.Views.InstrumentsDiscovery.InstrumentsDetailCell();
+            this.instrumentsDetailCell2 = new MeterKnife.Views.InstrumentsDiscovery.InstrumentsDetailCell();
             this.SuspendLayout();
+            // 
+            // instrumentsDetailCell1
+            // 
+            this.instrumentsDetailCell1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.instrumentsDetailCell1.ConnString = "GPIB0:22:INST";
+            this.instrumentsDetailCell1.DatasCount = "23";
+            this.instrumentsDetailCell1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.instrumentsDetailCell1.Font = new System.Drawing.Font("微软雅黑", 8.5F);
+            this.instrumentsDetailCell1.Image = null;
+            this.instrumentsDetailCell1.Information = ".......";
+            this.instrumentsDetailCell1.Location = new System.Drawing.Point(5, 5);
+            this.instrumentsDetailCell1.Manufacturer = "Aglient";
+            this.instrumentsDetailCell1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.instrumentsDetailCell1.Model = "E36103A";
+            this.instrumentsDetailCell1.Name = "instrumentsDetailCell1";
+            this.instrumentsDetailCell1.Size = new System.Drawing.Size(490, 64);
+            this.instrumentsDetailCell1.TabIndex = 0;
+            this.instrumentsDetailCell1.UsingTime = "2017/8/22";
+            // 
+            // instrumentsDetailCell2
+            // 
+            this.instrumentsDetailCell2.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.instrumentsDetailCell2.ConnString = "GPIB0:22:INST";
+            this.instrumentsDetailCell2.DatasCount = "23";
+            this.instrumentsDetailCell2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.instrumentsDetailCell2.Font = new System.Drawing.Font("微软雅黑", 8.5F);
+            this.instrumentsDetailCell2.Image = null;
+            this.instrumentsDetailCell2.Information = ".......";
+            this.instrumentsDetailCell2.Location = new System.Drawing.Point(5, 69);
+            this.instrumentsDetailCell2.Manufacturer = "Aglient";
+            this.instrumentsDetailCell2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.instrumentsDetailCell2.Model = "E36103A";
+            this.instrumentsDetailCell2.Name = "instrumentsDetailCell2";
+            this.instrumentsDetailCell2.Size = new System.Drawing.Size(490, 64);
+            this.instrumentsDetailCell2.TabIndex = 1;
+            this.instrumentsDetailCell2.UsingTime = "2017/8/22";
             // 
             // InstrumentsPanel
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.instrumentsDetailCell2);
+            this.Controls.Add(this.instrumentsDetailCell1);
             this.Name = "InstrumentsPanel";
-            this.Size = new System.Drawing.Size(400, 400);
+            this.Padding = new System.Windows.Forms.Padding(5);
+            this.Size = new System.Drawing.Size(500, 400);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private InstrumentsDetailCell instrumentsDetailCell1;
+        private InstrumentsDetailCell instrumentsDetailCell2;
     }
 }
