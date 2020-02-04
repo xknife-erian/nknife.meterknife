1a3cbd7ba07d8e7ff3f7cf86d11d5b524946e988
Erian-Home <lukan@xknife.net>
2020-02-05 01:05:05 +0800

    .

--- MeterKnife.CLI/Commands/CareCliCommand.cs
+++ MeterKnife.CLI/Commands/CareCliCommand.cs
@@ -39,11 +39,11 @@ namespace NKnife.MeterKnife.CLI.Commands
 
         private CareCommandPool GetCommands()
         {
-            var interval = 5000;
+            var interval = 1000;
             var item1 = new CareCommand
             {
                 Slot = _slot,
-                DUT = new DUT() {Id = "VOLTAGE", Name = "10v"},
+                DUT = new DUT() {Id = "RES", Name = "520r|1K"},
                 GpibAddress = 23,
                 Scpi = new Scpi {Command = "FETC?"},
 
@@ -54,7 +54,7 @@ namespace NKnife.MeterKnife.CLI.Commands
             var item2 = new CareCommand
             {
                 Slot = _slot,
-                DUT = new DUT() {Id = "RES", Name = "1k"},
+                DUT = new DUT() {Id = "VOLTAGE", Name = "10v" },
                 GpibAddress = 24,
                 Scpi = new Scpi {Command = "READ?"},
 
